bfb8e071a9162272533122ba81789055c8c78720
01-131172-042 Student BUIC <01-131172-042@student.bahria.edu.pk>
2019-11-24 22:12:01 +0500

    Pre processing on the image with basic user interface.

--- MCQ/Form1.Designer.cs
+++ MCQ/Form1.Designer.cs
@@ -34,12 +34,16 @@
             this.imageBox1 = new Emgu.CV.UI.ImageBox();
             this.label2 = new System.Windows.Forms.Label();
             this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.imageBox2 = new Emgu.CV.UI.ImageBox();
+            this.button4 = new System.Windows.Forms.Button();
             ((System.ComponentModel.ISupportInitialize)(this.imageBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.imageBox2)).BeginInit();
             this.SuspendLayout();
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(536, 31);
+            this.button1.Location = new System.Drawing.Point(36, 38);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(75, 23);
             this.button1.TabIndex = 0;
@@ -89,12 +93,45 @@
             this.button2.UseVisualStyleBackColor = true;
             this.button2.Click += new System.EventHandler(this.Button2_Click);
             // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(148, 38);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(101, 23);
+            this.button3.TabIndex = 5;
+            this.button3.Text = "Convert to binary";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.Button3_Click);
+            // 
+            // imageBox2
+            // 
+            this.imageBox2.BackColor = System.Drawing.Color.White;
+            this.imageBox2.Location = new System.Drawing.Point(36, 141);
+            this.imageBox2.Name = "imageBox2";
+            this.imageBox2.Size = new System.Drawing.Size(256, 249);
+            this.imageBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.imageBox2.TabIndex = 2;
+            this.imageBox2.TabStop = false;
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(293, 38);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(122, 23);
+            this.button4.TabIndex = 6;
+            this.button4.Text = "Addaptive Threshhold";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.Button4_Click);
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(30)))), ((int)(((byte)(28)))), ((int)(((byte)(38)))));
             this.ClientSize = new System.Drawing.Size(888, 469);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.imageBox2);
+            this.Controls.Add(this.button3);
             this.Controls.Add(this.button2);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.imageBox1);
@@ -104,6 +141,7 @@
             this.Name = "Form1";
             this.Text = "Form1";
             ((System.ComponentModel.ISupportInitialize)(this.imageBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.imageBox2)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -116,6 +154,9 @@
         private Emgu.CV.UI.ImageBox imageBox1;
         private System.Windows.Forms.Label label2;
         private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private Emgu.CV.UI.ImageBox imageBox2;
+        private System.Windows.Forms.Button button4;
     }
 }
 
--- MCQ/Form1.cs
+++ MCQ/Form1.cs
@@ -15,11 +15,12 @@ namespace MCQ
     public partial class Form1 : Form
     {
         string fileName = "mcqSample.jpg";
-        Image<Bgr, byte> img;
+        Image<Bgr, Byte> img;
+        Image<Gray, Byte> imgGray;
         public Form1()
         {
             InitializeComponent();
-            img = new Image<Bgr, byte>(fileName);
+            img = new Image<Bgr, Byte>(fileName);
             imageBox1.Image = img;
         }
 
@@ -33,11 +34,29 @@ namespace MCQ
         private void Button1_Click(object sender, EventArgs e)
         {
             OpenFileDialog ofd = new OpenFileDialog();
+            
             if(ofd.ShowDialog()==DialogResult.OK)
             {
-                img = new Image<Bgr, byte>(ofd.FileName);
+                fileName = ofd.FileName;
+                img = new Image<Bgr, Byte>(fileName);
+                ofd.Filter = " Image Files(*.tif;*.dcm;*.jpg;*.jpeg;*.bmp)|*.tif;*.dcm;*.jpg;*.jpeg;*.bmp";
                 imageBox1.Image = img;
             }
+        }
+
+        private void Button3_Click(object sender, EventArgs e)
+        {
+           imgGray = new Image<Gray, Byte>(fileName);
+           imageBox2.Image = imgGray;
+           imgGray = imgGray.ThresholdBinary(new Gray(120), new Gray(255));
+           imageBox2.Image = imgGray;
+        }
+
+        private void Button4_Click(object sender, EventArgs e)
+        {
+            CvInvoke.AdaptiveThreshold(imgGray, imgGray,255, Emgu.CV.CvEnum.AdaptiveThresholdType.GaussianC, Emgu.CV.CvEnum.ThresholdType.Binary, 75, 10);
+            CvInvoke.BitwiseNot(imgGray, imgGray);
+            imageBox2.Image = imgGray;
         }
     }
 }
