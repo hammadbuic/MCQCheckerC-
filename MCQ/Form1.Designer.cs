43bade68ea0f18db1ce3339864dd0528a42e4f72
01-131172-042 Student BUIC <01-131172-042@student.bahria.edu.pk>
2019-11-24 16:50:04 +0500

    Basic User Interface Design (Many Changes Required)

--- MCQ/Form1.Designer.cs
+++ MCQ/Form1.Designer.cs
@@ -29,35 +29,77 @@
         private void InitializeComponent()
         {
             this.components = new System.ComponentModel.Container();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
             this.imageBox1 = new Emgu.CV.UI.ImageBox();
+            this.label2 = new System.Windows.Forms.Label();
             ((System.ComponentModel.ISupportInitialize)(this.imageBox1)).BeginInit();
             this.SuspendLayout();
             // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(46, 27);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "browse";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.ForeColor = System.Drawing.SystemColors.ControlLight;
+            this.label1.Location = new System.Drawing.Point(32, 93);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(99, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Student Information";
+            // 
             // imageBox1
             // 
-            this.imageBox1.Location = new System.Drawing.Point(226, 55);
+            this.imageBox1.BackColor = System.Drawing.Color.White;
+            this.imageBox1.Location = new System.Drawing.Point(643, 119);
             this.imageBox1.Name = "imageBox1";
-            this.imageBox1.Size = new System.Drawing.Size(504, 277);
+            this.imageBox1.Size = new System.Drawing.Size(200, 150);
+            this.imageBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
             this.imageBox1.TabIndex = 2;
             this.imageBox1.TabStop = false;
-            this.imageBox1.Click += new System.EventHandler(this.ImageBox1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.ForeColor = System.Drawing.SystemColors.ControlLight;
+            this.label2.Location = new System.Drawing.Point(640, 83);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(173, 13);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Display Answer Sheet in image Box";
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(30)))), ((int)(((byte)(28)))), ((int)(((byte)(38)))));
+            this.ClientSize = new System.Drawing.Size(888, 469);
+            this.Controls.Add(this.label2);
             this.Controls.Add(this.imageBox1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
             this.Name = "Form1";
             this.Text = "Form1";
             ((System.ComponentModel.ISupportInitialize)(this.imageBox1)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
 
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
         private Emgu.CV.UI.ImageBox imageBox1;
+        private System.Windows.Forms.Label label2;
     }
 }
 
