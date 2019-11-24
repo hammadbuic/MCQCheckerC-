483ebb97919344e143a79a8d61c796fe6dbd69da
01-131172-042 Student BUIC <01-131172-042@student.bahria.edu.pk>
2019-11-24 19:45:54 +0500

    Basic User Interface

--- MCQ/Form1.Designer.cs
+++ MCQ/Form1.Designer.cs
@@ -39,47 +39,49 @@
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(46, 27);
+            this.button1.Location = new System.Drawing.Point(536, 31);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(75, 23);
             this.button1.TabIndex = 0;
             this.button1.Text = "browse";
             this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.Button1_Click);
             // 
             // label1
             // 
             this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label1.ForeColor = System.Drawing.SystemColors.ControlLight;
             this.label1.Location = new System.Drawing.Point(32, 93);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(99, 13);
+            this.label1.Size = new System.Drawing.Size(160, 19);
             this.label1.TabIndex = 1;
             this.label1.Text = "Student Information";
             // 
             // imageBox1
             // 
             this.imageBox1.BackColor = System.Drawing.Color.White;
-            this.imageBox1.Location = new System.Drawing.Point(643, 119);
+            this.imageBox1.Location = new System.Drawing.Point(536, 119);
             this.imageBox1.Name = "imageBox1";
-            this.imageBox1.Size = new System.Drawing.Size(200, 150);
-            this.imageBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
+            this.imageBox1.Size = new System.Drawing.Size(307, 271);
+            this.imageBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
             this.imageBox1.TabIndex = 2;
             this.imageBox1.TabStop = false;
-            this.imageBox1.Click += new System.EventHandler(this.ImageBox1_Click_1);
             // 
             // label2
             // 
             this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label2.ForeColor = System.Drawing.SystemColors.ControlLight;
-            this.label2.Location = new System.Drawing.Point(640, 83);
+            this.label2.Location = new System.Drawing.Point(532, 83);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(173, 13);
+            this.label2.Size = new System.Drawing.Size(278, 19);
             this.label2.TabIndex = 3;
             this.label2.Text = "Display Answer Sheet in image Box";
             // 
             // button2
             // 
-            this.button2.Location = new System.Drawing.Point(556, 429);
+            this.button2.Location = new System.Drawing.Point(536, 422);
             this.button2.Name = "button2";
             this.button2.Size = new System.Drawing.Size(75, 23);
             this.button2.TabIndex = 4;
--- MCQ/Form1.cs
+++ MCQ/Form1.cs
@@ -14,27 +14,30 @@ namespace MCQ
 {
     public partial class Form1 : Form
     {
+        string fileName = "mcqSample.jpg";
+        Image<Bgr, byte> img;
         public Form1()
         {
             InitializeComponent();
+            img = new Image<Bgr, byte>(fileName);
+            imageBox1.Image = img;
         }
 
-        private void ImageBox1_Click(object sender, EventArgs e)
-        {
-            
-        }
-
-        private void ImageBox1_Click_1(object sender, EventArgs e)
-        {
-            string fileName = "mcqSample.jpg";
-            Image<Bgr, byte> img = new Image<Bgr, byte>(fileName);
-            CvInvoke.Imshow("Image", img);
-            
-        }
+      
 
         private void Button2_Click(object sender, EventArgs e)
         {
             Application.Exit();
+        }
+
+        private void Button1_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog ofd = new OpenFileDialog();
+            if(ofd.ShowDialog()==DialogResult.OK)
+            {
+                img = new Image<Bgr, byte>(ofd.FileName);
+                imageBox1.Image = img;
+            }
         }
     }
 }
